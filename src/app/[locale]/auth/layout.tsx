3e3a603da11355f61734e0a0b6d6096246c73599
binarydata123 <binarydata.sale@gmail.com>
2025-08-19 17:53:30 +0530

    live code local to prod

--- src/app/[locale]/auth/layout.tsx
+++ src/app/[locale]/auth/layout.tsx
@@ -22,7 +22,8 @@ export default function AuthLayout({ children }: { children: React.ReactNode }) 
 										<img src='https://images.unsplash.com/photo-1573496359142-b8d87734a5a2?q=80&w=1976&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' className="sidebar-img" />
 									</>
 								) : pathname.includes('auth/lawyer') ? (
-									<img src="/auth-bg-img.jpg" id="background-video" alt="bg-image" />
+									// <img src="/auth-bg-img.jpg" id="background-video" alt="bg-image" />
+									<img src="/auth-bg-img.jpg"  alt="bg-image" />
 								) : (
 
 									<img src='https://images.unsplash.com/photo-1573496359142-b8d87734a5a2?q=80&w=1976&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' className="sidebar-img" />
